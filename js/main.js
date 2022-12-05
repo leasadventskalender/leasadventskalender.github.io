6cf6fd3d8db00b2165e7c069232f4cb7d9aa6668
Andi Balser <andreas.balser@stud-mail.uni-wuerzburg.de>
2022-12-05 13:10:01 +0100

    fix disabled popup bug again

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function disableDoors() {
 }
 
 function showPopup(number) {
-    if (document.getElementById('door' + (number-1)).classList.contains('disabled')) return;
+    if (document.getElementById('door' + number).classList.contains('disabled')) return;
 
     const popups = document.getElementById("popups");
 
